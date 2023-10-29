296d62d8b4241949a1d167a50315c1f2c3cc10b1
Tony Wang <Tony94042@gmail.com>
2023-10-29 14:26:26 +1100

    Link to Domain API working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchModule } from './search/search.module';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SearchModule } from './search/search.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SearchModule
+    SearchModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search-home/search-home.component.ts
+++ src/app/search/search-home/search-home.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-search-home',
   templateUrl: './search-home.component.html',
   styleUrls: ['./search-home.component.css']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class SearchHomeComponent {
   searchForm = new FormGroup({
     state: new FormControl(''),
     suburb: new FormControl(''),
-    postcode: new FormControl('')
+    postcode: new FormControl(''),
+    bedrooms: new FormControl(''),
+    type: new FormControl('')
   })
 
   result: any;
 
-  constructor(){
-    console.log(this.searchForm)
+  constructor(private http: HttpClient){
   }
 
   storeFormsValue(){
-    this.result = this.searchForm.value
-    console.log(this.result);
+    const {state, suburb, postcode, type, bedrooms} = this.searchForm.value;
+    const apiKey = 'key_16e9a90ece4b1760855b15b81a51a707';
+    const headers = new HttpHeaders().set('X-API-KEY', apiKey);
+
+    return this.http.get<any>(`https://api.domain.com.au/v2/suburbPerformanceStatistics/${state}/${suburb}/${postcode}?propertyCategory=${type}&bedrooms=${bedrooms}&periodSize=years&startingPeriodRelativeToCurrent=1&totalPeriods=1`, {headers}).subscribe(
+      response => {
+        this.result = response.series.seriesInfo[0].values;
+        console.log(this.result);
+      }
+    )
   }
 }
