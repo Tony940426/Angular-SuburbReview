33df2deb5fa2de7b76949cd91187742d82fb4ace
Tony Wang <Tony94042@gmail.com>
2023-10-29 12:48:04 +1100

    Added Module and set up basic template for forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchHomeComponent } from './module/search-home/search-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchHomeComponent
   ],
   imports: [
     BrowserModule,
